537d02dd1cf0826e05cd8ef0e68bb868d75f5ce2
idosakazi123 <idosakazi@gmail.com>
2018-11-22 21:33:19 +0200

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
 import $ from 'jquery';
-import {parseCode} from './code-analyzer';
+import {parseCode,getElementFromParseCode,createTable} from './code-analyzer';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
+        //$('#parsedCode').text(parsedCode);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        $('#viewTable').empty();
+        $('#viewTable').append(createTable(getElementFromParseCode(parsedCode)));
+
     });
 });
+
